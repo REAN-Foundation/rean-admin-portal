3f488ed6fab8103098a0be88d8520419108e736f
Dattatraya-Londhe <londhedn@gmail.com>
2024-06-15 14:26:36 +0530

    Fixed priority & goal delete issue.

--- src/routes/users/[userId]/goals/+page.svelte
+++ src/routes/users/[userId]/goals/+page.svelte
@@ -91,6 +91,11 @@
 	};
 
 	async function Delete(model) {
+        const response = await fetch(`/api/server/goals`, {
+            method: 'DELETE',
+            body: JSON.stringify(model),
+            headers: { 'content-type': 'application/json' }
+        });
 	}
 </script> 
 
