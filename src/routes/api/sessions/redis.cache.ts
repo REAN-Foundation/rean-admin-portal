bbc4c466b0de9fcac07f6746ea4c8b1573f38842
Dattatraya-Londhe <londhedn@gmail.com>
2024-07-04 14:33:13 +0530

    Code clean up.

--- src/routes/api/sessions/redis.cache.ts
+++ src/routes/api/sessions/redis.cache.ts
@@ -2,31 +2,7 @@ import { type Session } from "./session";
 import { type ISessionCache } from "./session.cache.interface";
 import { createClient, type RedisClientType } from 'redis';
 import { SESSION_CACHE_HOST, SESSION_CACHE_PASSWORD } from "$env/static/private";
-////////////////////////////////////////////////////////////////////////////////////////
-// Using KeyDB as a Redis cache
-// KeyDB is a high-performance fork of Redis with a focus on multithreading and memory efficiency
-// It is designed to be a drop-in replacement for Redis
-// KeyDB is fully compatible with Redis and supports all Redis commands
-// 
-// docker run \
-// -d --name keydb \
-// -p 6379:6379 \
-// -e "SESSION_CACHE_PASSWORD=your-password" \
-// -v /path/to/your/data:/data \
-// -v /path/to/your/logs:/logs keydb/keydb \
-// eqalpha/keydb
 
-// Process to connect with KeyDB is same as Redis.
-// 1. Run the docker container.
-// 2. Set the password by logging into container
-//    a. First run redis-cli as 
-//       ```# redis-cli```
-//    b. Set the password using
-//       ```# auth <your-password>```
-// 3. Create a client and connect to KeyDB.
-// 4. Use the client to perform operations.
-// 5. Close the connection when done.
-//
 ////////////////////////////////////////////////////////////////////////////////////////
 
 export class RedisCache implements ISessionCache {
