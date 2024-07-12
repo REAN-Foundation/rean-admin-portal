9caeaa04caf43b668e1b211b9738f3478ab655cc
nisha-inflectionzone <nisha.londhe@inflectionzone.com>
2024-07-12 16:13:11 +0530

    date refined n handleDateSubmission

--- src/routes/api/services/gghn/appointment-set.ts
+++ src/routes/api/services/gghn/appointment-set.ts
@@ -2,7 +2,24 @@ import { GMU_URL } from '$env/static/private';
 const gmuUrl = GMU_URL;
 
 export async function handleDateSubmission(date: string) {
-    const url = new URL(`${gmuUrl}/appointment-schedules/gghn/set-reminders/date/${date}`);
+   let dateStr = date
+   console.log("data...*...",date)
+   let d = date.split('-')
+   console.log("data1",d[0])
+   console.log("data2",d[1])
+   console.log("data3",d[2])
+   if(d[0].startsWith('0') || d[2].startsWith('0')){
+    let datefirst = parseInt(d[0], 10);
+    let datelast = parseInt(d[2], 10);
+    dateStr = (`${datefirst}-${d[1]}-${datelast}`)
+    console.log("dateStr....",dateStr)
+   }
+   else{
+    dateStr = date
+    console.log("dateStr....",dateStr)
+   }
+    // const dateString = dateStr
+    const url = new URL(`${gmuUrl}/appointment-schedules/gghn/set-reminders/date/${dateStr}`);
     // url.searchParams.append('date', date);
 
   try {
