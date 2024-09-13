7a113c3cfbd443914dd22d0553f73225e7f11407
rf-opssupport <84499879+rf-opssupport@users.noreply.github.com>
2024-09-13 15:58:13 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,6 +38,7 @@ aws s3 cp s3://$ENV_FILE_BUCKET/.env ./.env
 aws s3 cp s3://$CONSTANTS_FILE_BUCKET/constants.ts ./src/lib/constants.ts
 aws s3 cp s3://$FAVICON_FILE_BUCKET/favicon.png ./static/favicon.png
 
+chmod +x ./src/lib/constants.ts
 cd /app/build
 # Add any other scripts here...
 
