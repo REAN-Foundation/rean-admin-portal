f55cc01fd80138ccabf3a4f4a8f6b98e70547800
rf-opssupport <84499879+rf-opssupport@users.noreply.github.com>
2024-09-16 12:25:36 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
-ENVIRONMENT=$1
-# Determine the environment and set S3 paths accordingly
-case "$ENVIRONMENT" in
-  "dev")
-    ENV_FILE_BUCKET="duploservices-dev-configs-new-167414264568/rean-admin-portal"
-    CONSTANTS_FILE_BUCKET="duploservices-dev-configs-new-167414264568/rean-admin-portal"
-    FAVICON_FILE_BUCKET="duploservices-dev-configs-new-167414264568/rean-admin-portal"
-    ;;
-  "uat")
-    ENV_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal"
-    CONSTANTS_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal"
-    FAVICON_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal"
-    ;;
-  "prod")
-    ENV_FILE_BUCKET="duploservices-prod-configs-new-167414264568/rean-admin-portal"
-    CONSTANTS_FILE_BUCKET="duploservices-prod-configs-new-167414264568/rean-admin-portal"
-    FAVICON_FILE_BUCKET="duploservices-prod-configs-new-167414264568/rean-admin-portal"
-    ;;
-  "aha-prod")
-    ENV_FILE_BUCKET="duploservices-ahaprod-configs-167414264568/rean-admin-portal"
-    CONSTANTS_FILE_BUCKET="duploservices-ahaprod-configs-167414264568/rean-admin-portal"
-    FAVICON_FILE_BUCKET="duploservices-ahaprod-configs-167414264568/rean-admin-portal"
-    ;;
-  "aha-uat")
-    ENV_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal-aha"
-    CONSTANTS_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal-aha"
-    FAVICON_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal-aha"
-    ;;
-  *)
-    echo "Unknown environment: $ENVIRONMENT"
-    exit 1
-    ;;
-esac
+# ENVIRONMENT=$1
+# # Determine the environment and set S3 paths accordingly
+# case "$ENVIRONMENT" in
+#   "dev")
+#     ENV_FILE_BUCKET="duploservices-dev-configs-new-167414264568/rean-admin-portal"
+#     CONSTANTS_FILE_BUCKET="duploservices-dev-configs-new-167414264568/rean-admin-portal"
+#     FAVICON_FILE_BUCKET="duploservices-dev-configs-new-167414264568/rean-admin-portal"
+#     ;;
+#   "uat")
+#     ENV_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal"
+#     CONSTANTS_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal"
+#     FAVICON_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal"
+#     ;;
+#   "prod")
+#     ENV_FILE_BUCKET="duploservices-prod-configs-new-167414264568/rean-admin-portal"
+#     CONSTANTS_FILE_BUCKET="duploservices-prod-configs-new-167414264568/rean-admin-portal"
+#     FAVICON_FILE_BUCKET="duploservices-prod-configs-new-167414264568/rean-admin-portal"
+#     ;;
+#   "aha-prod")
+#     ENV_FILE_BUCKET="duploservices-ahaprod-configs-167414264568/rean-admin-portal"
+#     CONSTANTS_FILE_BUCKET="duploservices-ahaprod-configs-167414264568/rean-admin-portal"
+#     FAVICON_FILE_BUCKET="duploservices-ahaprod-configs-167414264568/rean-admin-portal"
+#     ;;
+#   "aha-uat")
+#     ENV_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal-aha"
+#     CONSTANTS_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal-aha"
+#     FAVICON_FILE_BUCKET="duploservices-uat-configs-new-167414264568/rean-admin-portal-aha"
+#     ;;
+#   *)
+#     echo "Unknown environment: $ENVIRONMENT"
+#     exit 1
+#     ;;
+# esac
 
 # Copy files from S3
 aws s3 cp s3://$ENV_FILE_BUCKET/.env ./.env
