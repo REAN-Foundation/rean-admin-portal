658ed5b0472c24bad928e17fe3aa4916324da8f8
RupaliD <rupali.dinde@inflectionzone.com>
2024-10-04 18:01:11 +0530

    Added error msg for duplicate tags

--- src/lib/components/input-chips.svelte
+++ src/lib/components/input-chips.svelte
@@ -24,34 +24,45 @@
 	const cInputField = 'unstyled bg-transparent border-0 !ring-0 p-0 pl-1 w-full';
 	let inputValue = '';
 	let inputValid = true;
+	let errorMessage = '';
+
 	function onInputHandler() {
-		inputValid = true;
+			inputValid = true;
+			errorMessage = ''; 
 	}
+
 	function validate() {
-		if (!inputValue) return false;
-		if (validation !== void 0 && !validation()) return false;
-		if (max !== -1 && value.length >= max) return false;
-		if (minlength !== -1 && inputValue.length < minlength) return false;
-		if (maxlength !== -1 && inputValue.length > maxlength) return false;
-		if (whitelist.length > 0 && !whitelist.includes(inputValue)) return false;
-		if (allowDuplicates === false && value.includes(inputValue)) return false;
-		return true;
+			if (!inputValue) return false;
+			if (validation !== void 0 && !validation()) return false;
+			if (max !== -1 && value.length >= max) return false;
+			if (minlength !== -1 && inputValue.length < minlength) return false;
+			if (maxlength !== -1 && inputValue.length > maxlength) return false;
+			if (whitelist.length > 0 && !whitelist.includes(inputValue)) return false;
+			if (allowDuplicates === false && value.includes(inputValue)) {
+					errorMessage = 'Tag is already added';
+					return false;
+			}
+			return true;
 	}
+
 	function addChip(event) {
-		event.preventDefault();
-		inputValid = validate();
-		if (inputValid === false) return;
-		inputValue = inputValue.trim();
-		inputValue = allowUpperCase ? inputValue : inputValue.toLowerCase();
-		value = [...value, inputValue];
-		dispatch('add', { event, chipIndex: value.length - 1, chipValue: inputValue });
-		inputValue = '';
+			event.preventDefault();
+			inputValid = validate();
+			if (inputValid === false) return;
+			
+			inputValue = inputValue.trim();
+			inputValue = allowUpperCase ? inputValue : inputValue.toLowerCase();
+			value = [...value, inputValue];
+			dispatch('add', { event, chipIndex: value.length - 1, chipValue: inputValue });
+			inputValue = '';
 	}
+
 	function removeChip(event, chipIndex, chipValue) {
-		if ($$restProps.disabled) return;
-		value = value.filter((_, i) => i !== chipIndex);
-		dispatch('remove', { event, chipIndex, chipValue });
+			if ($$restProps.disabled) return;
+			value = value.filter((_, i) => i !== chipIndex);
+			dispatch('remove', { event, chipIndex, chipValue });
 	}
+
 	$: classesInvalid = inputValid === false ? invalid : '';
 	$: classesBase = `${cBase} ${padding} ${rounded} ${$$props.class ?? ''} ${classesInvalid}`;
 	$: classesInterface = `${cInterface}`;
@@ -60,58 +71,48 @@
 </script>
 
 <div class="input-chip {classesBase}" class:opacity-50={$$restProps.disabled}>
-	<!-- NOTE: Don't use `hidden` as it prevents `required` from operating -->
 	<div class="h-0 overflow-hidden">
-		<select bind:value {name} multiple {required} tabindex="-1">
-			<!-- NOTE: options are required! -->
-			{#each value as option}<option value={option}>{option}</option>{/each}
-		</select>
+			<select bind:value {name} multiple {required} tabindex="-1">
+					{#each value as option}<option value={option}>{option}</option>{/each}
+			</select>
 	</div>
-	<!-- Interface -->
 	<div class="input-chip-interface {classesInterface}">
-		<!-- Input Field -->
-		<form on:submit={addChip}>
-			<input
-				type="text"
-				bind:value={inputValue}
-				placeholder={$$restProps.placeholder ?? 'To add a tag, type and press enter.'}
-				class="input-chip-field {classesInputField}"
-				on:input={onInputHandler}
-				on:input
-				disabled={$$restProps.disabled}
-			/>
-		</form>
+			<form on:submit={addChip}>
+					<input
+							type="text"
+							bind:value={inputValue}
+							placeholder={$$restProps.placeholder ?? 'To add a tag, type and press enter.'}
+							class="input-chip-field {classesInputField}"
+							on:input={onInputHandler}
+							disabled={$$restProps.disabled}
+					/>
+			</form>
 
-		<!-- Chip List -->
-		{#if value.length}
-			<div
-				class="input-chip-list {classesChipList}"
-				transition:fly|local={{ duration, opacity: 0, y: -20 }}
-			>
-				{#each value as c, i (c)}
-					<!-- Wrapping div required for FLIP animation -->
-					<div animate:flip={{ duration }}>
-						<button
-							class="chip {chips}"
-							on:click={(e) => {
-								e.preventDefault();
-								removeChip(e, i, c);
-							}}
-							on:click
-							on:keypress
-							on:keydown
-							on:keyup
-							transition:scale|local={{ duration, opacity: 0 }}
-						>
-							<span>{c}</span>
-							<span>✕</span>
-						</button>
+			{#if value.length}
+					<div class="input-chip-list {classesChipList}" transition:fly|local={{ duration, opacity: 0, y: -20 }}>
+							{#each value as c, i (c)}
+									<div animate:flip={{ duration }}>
+											<button
+													class="chip {chips}"
+													on:click={(e) => {
+															e.preventDefault();
+															removeChip(e, i, c);
+													}}
+													transition:scale|local={{ duration, opacity: 0 }}
+											>
+													<span>{c}</span>
+													<span>✕</span>
+											</button>
+									</div>
+							{/each}
+							{#if value.length > 9}
+									<div class="text-error-500">Number of tags should be less than or equal to 10</div>
+							{/if}
 					</div>
-				{/each}
-				{#if value.length > 9 }
-				<div class="text-error-500">Number of tags should be less than or equal to 10</div>
-				{/if}
-			</div>
-		{/if}
+			{/if}
+			
+			{#if errorMessage}
+					<div class="text-error-500">{errorMessage}</div>
+			{/if}
 	</div>
 </div>
