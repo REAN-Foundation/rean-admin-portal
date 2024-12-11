532ee1a29bc3aa74aa2aef0a8a47b8857e37e753
RupaliD <rupali.dinde@inflectionzone.com>
2024-12-11 12:27:52 +0530

    Performace fixes for dashboard

--- src/routes/users/[userId]/home/generic-engagement/+page.server.ts
+++ src/routes/users/[userId]/home/generic-engagement/+page.server.ts
@@ -4,6 +4,8 @@ import { getUserAnalytics } from '../../../../api/services/user-analytics/user-a
 import chalk from 'chalk';
 import { redirect } from 'sveltekit-flash-message/server';
 import { errorMessage } from '$lib/utils/message.utils';
+import { TimeHelper } from '$lib/utils/time.helper';
+import { DateStringFormat } from '$lib/types/time.types';
 
 ////////////////////////////////////////////////////////////////////////////
 
@@ -11,16 +13,8 @@ export const load: PageServerLoad = async (event: RequestEvent) => {
     const sessionId = event.cookies.get('sessionId');
     const userId = event.params.userId;
     const today = new Date();
-    const yyyy = today.getFullYear();
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-    const dd = String(today.getDate()).padStart(2, '0');
-
-    const formattedDate = `${yyyy}-${mm}-${dd}`;
+    const formattedDate = TimeHelper.getDateString(today, DateStringFormat.YYYY_MM_DD);
     const response = await getUserAnalytics(sessionId, formattedDate)
-    console.log(chalk.yellow(JSON.stringify(response)));
-    // const data = JSON.parse(await response);
-
-
     if (!response) {
         throw error(404, 'Daily user statistics data not found');
     }
@@ -30,11 +24,10 @@ export const load: PageServerLoad = async (event: RequestEvent) => {
             event
         );
     }
-
-
+    const genericMetrics = response.Data.GenericMetrics;
     return {
         sessionId,
-        statistics: response.Data,
+        genericMetrics,
         title:'Dashboard-Home-Generic'
     };
 };
